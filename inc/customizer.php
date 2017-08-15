bebaa4abddd30eaf87bfbd730b0f0b60f25db0a6
Paul M Dorr <paul.mdorr@gmail.com>
2017-08-15 01:25:57 -0300

    Add setting to show or hide comments link

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,23 +10,44 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function medium_rare_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+function medium_rare_customize_register($wp_customize) {
+	$wp_customize->get_setting('blogname')->transport         = 'postMessage';
+	$wp_customize->get_setting('blogdescription')->transport  = 'postMessage';
+	$wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+	if (isset($wp_customize->selective_refresh)) {
+		$wp_customize->selective_refresh->add_partial('blogname', array(
 			'selector'        => '.site-title a',
 			'render_callback' => 'medium_rare_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+		));
+		$wp_customize->selective_refresh->add_partial('blogdescription', array(
 			'selector'        => '.site-description',
 			'render_callback' => 'medium_rare_customize_partial_blogdescription',
-		) );
+		));
 	}
+
+	// Show or hide "Leave a comment" link on the posts list
+  $wp_customize->add_setting('medium_rare_show_comments_link', array(
+		'type' => 'theme_mod',
+		'capability' => 'edit_theme_options',
+		'default' => 0,
+		'transport' => 'refresh',
+	));
+	$wp_customize->add_section('medium_rare', array(
+		'title' => __('Medium Rare Settings'),
+		'description' => __('Change theme behaviour here'),
+		'priority' => 10,
+		'capability' => 'edit_theme_options',
+	));
+	$wp_customize->add_control('medium_rare_show_comments_link', array(
+		'type' => 'checkbox',
+		'priority' => 10,
+		'section' => 'medium_rare',
+		'label' => __('Show comments link'),
+		'description' => __('Shows or hides the comments link in posts list.')
+	));
 }
-add_action( 'customize_register', 'medium_rare_customize_register' );
+add_action('customize_register', 'medium_rare_customize_register');
 
 /**
  * Render the site title for the selective refresh partial.
@@ -34,7 +55,7 @@ add_action( 'customize_register', 'medium_rare_customize_register' );
  * @return void
  */
 function medium_rare_customize_partial_blogname() {
-	bloginfo( 'name' );
+	bloginfo('name');
 }
 
 /**
@@ -43,13 +64,13 @@ function medium_rare_customize_partial_blogname() {
  * @return void
  */
 function medium_rare_customize_partial_blogdescription() {
-	bloginfo( 'description' );
+	bloginfo('description');
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function medium_rare_customize_preview_js() {
-	wp_enqueue_script( 'medium-rare-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script('medium-rare-customizer', get_template_directory_uri() . '/js/customizer.js', array('customize-preview'), '20151215', true);
 }
-add_action( 'customize_preview_init', 'medium_rare_customize_preview_js' );
+add_action('customize_preview_init', 'medium_rare_customize_preview_js');
