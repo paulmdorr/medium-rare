397c33ef19359e496e071cbcff9d38151b7dcdd2
Paul M Dorr <paul.mdorr@gmail.com>
2017-08-16 00:33:21 -0300

    Fix comment form inputs style discrepancies

--- js/comments.js
+++ js/comments.js
@@ -32,23 +32,22 @@ jQuery(document).ready(function($) {
 
   // Reposition name and email inputs
   // TODO: maybe there's a better way of doing this
-  $('.comment-form-comment').after('<p class="name-email-wrapper"></p>')
+  $('.comment-form-comment').after('<div class="name-email-wrapper"></div>')
   $('.name-email-wrapper').append($('.comment-form-author')).append($('.comment-form-email'))
 
   // Reposition submit button and logged in user element (if it exists)
   $('.comment-form-comment').append($('.form-submit input'))
   $('#comment').before($('.logged-in-user'))
   $('.form-submit').remove()
+  var originalCommentHeight = $('.comment-form-comment').css('height')
 
   // Change rows amount and show "Post Comment" button and user avatar depending on focus
   $('#comment').attr('rows', 1)
   $('#submit').hide()
   $('.logged-in-user').hide()
-  
-  $('#submit').click(function() {
-    console.log('test')
-  })
+  // Show comment textarea
   $("#comment").focusin(function() {
+    $('.comment-form-comment').css('height', 'auto')
     $("#comment").animate({
       rows: '+=5'
     }, 100, function() {
@@ -56,13 +55,17 @@ jQuery(document).ready(function($) {
       $('.logged-in-user').show()
     })
   })
+  // Hide comment textarea
   $("#comment").focusout(function() {
+    // The timeout avoids the Submit button click being prevented by the focusout event
     window.setTimeout(function() {
       $('#submit').hide()
       $('.logged-in-user').hide()
       $("#comment").animate({
         rows: '-=5'
-      }, 100)
+      }, 100, function() {
+        $('.comment-form-comment').css('height', originalCommentHeight)
+      })
     }, 100)
   })
 })
