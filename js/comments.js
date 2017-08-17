8267941555b85a38945f765d45fe02e3fb831946
Paul M Dorr <paul.mdorr@gmail.com>
2017-08-17 00:47:13 -0300

    Add support for basic HTML5 form validation

--- js/comments.js
+++ js/comments.js
@@ -2,6 +2,7 @@
  * File comments.js.
  *
  * Comments enhancements for a better user experience.
+ * TODO: I will probably reimplement this code using ES6 with babel
  *
  */
 
@@ -41,33 +42,48 @@ jQuery(document).ready(function($) {
   $('.form-submit').remove()
   var originalCommentHeight = $('.comment-form-comment').css('height')
 
+  this.tryingToSubmit = false
+  $('#submit').click(function() {
+    this.tryingToSubmit = true
+    window.setTimeout(function() {
+      this.tryingToSubmit = false
+    }.bind(this), 110)
+  }.bind(this))
+
   // Change rows amount and show "Post Comment" button and user avatar depending on focus
+  this.commentInputShowing = false
   $('#comment').attr('rows', 1)
   $('#submit').hide()
-  $('.user-info').hide()
+  $('.user-info').not('.vcard').hide()
   // Show comment textarea
   $("#comment").focusin(function() {
-    $('.comment-form-comment').css('height', 'auto')
-    $("#comment").animate({
-      rows: '+=5'
-    }, 100, function() {
-      $('#submit').show()
-      $('.user-info').show()
-    })
-  })
+    if (!this.commentInputShowing) {
+      $('.comment-form-comment').css('height', 'auto')
+      $("#comment").animate({
+        rows: '+=5'
+      }, 100, function() {
+        $('#submit').show()
+        $('.user-info').show()
+        this.commentInputShowing = true
+      }.bind(this))
+    }
+  }.bind(this))
   // Hide comment textarea
   $("#comment").focusout(function() {
     // The timeout avoids the Submit button click being prevented by the focusout event
     window.setTimeout(function() {
-      $('#submit').hide()
-      $('.user-info').hide()
-      $("#comment").animate({
-        rows: '-=5'
-      }, 100, function() {
-        $('.comment-form-comment').css('height', originalCommentHeight)
-      })
-    }, 100)
-  })
+      if (!this.tryingToSubmit) {
+        $('#submit').hide()
+        $('.user-info').not('.vcard').hide()
+        $("#comment").animate({
+          rows: '-=5'
+        }, 100, function() {
+          $('.comment-form-comment').css('height', originalCommentHeight)
+          this.commentInputShowing = false
+        }.bind(this))
+      }
+    }.bind(this), 100)
+  }.bind(this))
 
   // Restructure user info in comments
   $.each($('.comment-author'), function(index, value) {
@@ -88,4 +104,6 @@ jQuery(document).ready(function($) {
       $(value).find('.reply').hide()
     })
   })
+
+  $('form').removeAttr('novalidate')
 })
