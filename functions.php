78fc213cf1f4337bfef8d6a17a7da5410a71e293
Paul M Dorr <paul.mdorr@gmail.com>
2017-08-22 18:55:15 -0300

    Add menus support for mobile

--- functions.php
+++ functions.php
@@ -118,8 +118,8 @@ add_action('widgets_init', 'medium_rare_widgets_init');
  */
 function medium_rare_scripts() {
 	wp_enqueue_style('medium-rare-style', get_stylesheet_uri());
-	wp_enqueue_script('medium-rare-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
-	wp_enqueue_script('medium-rare-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
+	//wp_enqueue_script('medium-rare-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true);
+	//wp_enqueue_script('medium-rare-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
